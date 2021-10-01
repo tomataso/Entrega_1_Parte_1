89329fb7f82ddc0537922336b210458dca08cbf3
Juan Carlos <tomatejuan@gmail.com>
2021-09-30 22:12:38 -0600

    Archivo Generado en Camunda.com
    +Se descomprimio archivo aca.

---
+++ primer-workflow/src/main/java/cr/ac/cenfotec/discontextual/Application.java
@@ -0,0 +1,13 @@
+package cr.ac.cenfotec.discontextual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+  public static void main(String... args) {
+    SpringApplication.run(Application.class, args);
+  }
+
+}
